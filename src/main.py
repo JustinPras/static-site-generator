7eec58bc382900fea7f37afe06d0c3eeafcd19ad
JustinPras <justinponpras@gmail.com>
2024-12-28 16:26:55 +1100

    added main.py and textnode.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    text_node = TextNode("This is a text node", TextType.NORMAL_TEXT, "https://www.boot.dev")
+    print(text_node)
+
+if __name__ == "__main__":
+    main()
